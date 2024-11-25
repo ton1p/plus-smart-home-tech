032c24d47aa843ee4572c23490c03ebc9089444b
ton1p <posohin.tosha@yandex.ru>
2024-11-25 15:19:07 +0300

    feat: add grpc event handlers for sensors

--- telemetry/collector/src/main/java/collector/event/model/sensor/handler/MotionSensorEventHandler.java
+++ telemetry/collector/src/main/java/collector/event/model/sensor/handler/MotionSensorEventHandler.java
@@ -1,10 +1,22 @@
 package collector.event.model.sensor.handler;
 
+import collector.event.kafka.KafkaClient;
+import collector.event.kafka.KafkaTopics;
+import lombok.RequiredArgsConstructor;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+import ru.yandex.practicum.kafka.telemetry.event.MotionSensorAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
+import java.time.Instant;
 
 @Component
+@RequiredArgsConstructor
 public class MotionSensorEventHandler implements SensorEventHandler {
+    private final KafkaClient kafkaClient;
+
     @Override
     public SensorEventProto.PayloadCase getPayloadCase() {
         return SensorEventProto.PayloadCase.MOTION;
@@ -12,6 +24,18 @@ public class MotionSensorEventHandler implements SensorEventHandler {
 
     @Override
     public void handle(SensorEventProto event) {
+        SensorEventAvro sensorEventAvro = SensorEventAvro.newBuilder()
+                .setHubId(event.getHubId())
+                .setId(event.getId())
+                .setTimestamp(Instant.ofEpochSecond(event.getTimestamp().getSeconds()))
+                .setPayload(new MotionSensorAvro(
+                        event.getMotion().getLinkQuality(),
+                        event.getMotion().getMotion(),
+                        event.getMotion().getVoltage()
+                ))
+                .build();
 
+        ProducerRecord<String, SpecificRecordBase> producerRecord = new ProducerRecord<>(KafkaTopics.SENSORS, sensorEventAvro.getHubId(), sensorEventAvro);
+        kafkaClient.getProducer().send(producerRecord);
     }
 }
